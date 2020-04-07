fe4b5f532ba6dba97e801564a9f9bb6f31240669
MarioMorales7x7 <mario.morales.texas@gmail.com>
2020-04-06 21:13:20 -0500

    restarting MVP

---
+++ src/main/java/com/spring/polls/SpringPollApplication.java
@@ -0,0 +1,13 @@
+package com.spring.polls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPollApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/polls/SpringPollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.polls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
